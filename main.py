4ff3c26559d6639a1e8c07c5437f9845deff925b
Josimar Reyes Campos <darth_jos@hotmail.com>
2024-01-29 22:06:57 -0600

    29-01 | Josimar RC | Add commentary and default case functionality

--- main.py
+++ main.py
@@ -2,23 +2,27 @@ todos = []
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
-    user_action = user_action.lower().strip()
+    user_action = user_action.lower().strip()  # .strip(): sin argumentos quita los espacios al final en una cadena, con argumentos quita el simbolo parametrizado
 
+    # match: para comparar strings y se usan case para cada caso encontrado
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
-        case 'show':
+        case 'show' :
             for id, item in enumerate(todos):
-                print(f"{id+1}. {item}")
+                print(f"{id + 1}. {item}")
         case 'edit':
             print("Got it!")
             index_to_edit = int(input("Number of the todo to edit: ")) - 1
             todos[index_to_edit] = input(f"Type new todo for '{todos[index_to_edit]}': ")
         case 'complete':
-            index_to_remove = int(input("Number of the todo to complete: ")) -1
+            index_to_remove = int(input("Number of the todo to complete: ")) - 1
             todos.pop(index_to_remove)
         case 'exit':
             break
+        # Esto es como el case default, pero en vez de default, añadimos una variable vacía
+        case _:
+            print("You entered an unknown command...")
 
 print("Bye!")
