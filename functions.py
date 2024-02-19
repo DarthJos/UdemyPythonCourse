86b15963c1c35863cdc5add90173cb1f15eca823
Josimar Reyes Campos <darth_jos@hotmail.com>
2024-02-19 17:43:00 -0600

    19-02 | Day 14 | TODO-App | Decoupling bonus functions in modules

--- functions.py
+++ functions.py
@@ -10,3 +10,6 @@ def write_todos(todos_arg, local_filepath='todos.txt'):
     """Write the to-do items list in the text file."""
     with open(local_filepath, 'w') as file:
         file.writelines(todos_arg)
+
+if __name__ == "__main__":
+    print("\n ==== Esto solo se ejecuta cuando este archivo se ejecuta a sí mismo. ====")
