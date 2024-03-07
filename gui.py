56402e33832aa26d361065f29d312b08ecb2bc67
Josimar Reyes Campos <darth_jos@hotmail.com>
2024-03-07 13:53:11 -0600

    07-03 | Day 17 | Todo-App | Upgrade file compressor example

--- gui.py
+++ gui.py
@@ -6,7 +6,7 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(),
-                      key="todos",
+                      key='todos',
                       enable_events=True,
                       size=[45,10])
 edit_button = sg.Button("Edit")
