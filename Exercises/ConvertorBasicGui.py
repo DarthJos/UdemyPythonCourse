4745aab42697316a962f531bd8d7a57c0e7da155
Josimar Reyes Campos <darth_jos@hotmail.com>
2024-03-08 13:34:01 -0600

    08-03 | Day 17 | Todo-App | Add functionality to converter gui

--- Exercises/ConvertorBasicGui.py
+++ Exercises/ConvertorBasicGui.py
@@ -1,18 +1,35 @@
 import PySimpleGUI as sg
+import BonusExamples.ConverterMethods as converter
 
 window_title = "Convertor"
-enter_fee_label = sg.Text("Enter feet:")
+
+enter_feet_label = sg.Text("Enter feet:")
 enter_inches_label = sg.Text("Enter inches:")
-enter_fee_input = sg.Input()
-enter_inches_input = sg.Input()
-convert_button = sg.Button("Convert")
+
+enter_feet_input = sg.Input(key="feet_input")
+enter_inches_input = sg.Input(key="inches_input")
+
+convert_button = sg.Button("Convert", key="convert_btn")
+output_label = sg.Text(key="output")
 
 window = sg.Window(window_title,
                    layout=[
-                       [enter_fee_label, enter_fee_input],
+                       [enter_feet_label, enter_feet_input],
                        [enter_inches_label, enter_inches_input],
-                       [convert_button]
+                       [convert_button, output_label]
                    ])
 
-window.read()
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+    feet = float(values['feet_input'])
+    inches = float(values['inches_input'])
+
+    meters = converter.convert(feet, inches)
+    window['output'].update(value=f"{meters} m")
+
+
 window.close()
