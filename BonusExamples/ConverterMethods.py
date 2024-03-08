afe93c8b7747561a5eb79990bfe90c92143a7c6f
Josimar Reyes Campos <darth_jos@hotmail.com>
2024-03-08 13:44:16 -0600

    08-03 | Day 17 | Todo-App | Add convert_to_millimeters method

--- BonusExamples/ConverterMethods.py
+++ BonusExamples/ConverterMethods.py
@@ -1,3 +1,10 @@
 def convert_to_meters(feet, inches):
     meters = feet * 0.3048 + inches * 0.0254
     return meters
+
+
+def convert_to_millimeters(fluid_ounces):
+    equivalence = 29.57353
+
+    millimeters = fluid_ounces * equivalence
+    return millimeters
